23b2c75d8194f872a536b829990c9f90c451e1c6
ocowchun <ocowchun@gmail.com>
2015-03-02 19:37:40 +0800

    fix js for turbo link

--- app/assets/javascripts/homework_form.js
+++ app/assets/javascripts/homework_form.js
@@ -1,7 +1,7 @@
-$(function() {
+$(document).on("ready page:load", function() {
 	var $homework_deadline = $('#homework_deadline');
 	var options = {
 		dateFormat: 'yy-mm-dd'
 	};
 	$homework_deadline.datepicker(options);
-})
+});
