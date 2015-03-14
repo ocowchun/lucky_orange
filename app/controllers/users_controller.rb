005b0c91075ed277890daca683e01d279b2bf19a
ocowchun <ocowchun@gmail.com>
2015-03-14 13:11:55 +0800

    顯示使用者完成的作業

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,6 @@ class UsersController < ApplicationController
 
   def show
     @user=User.find(params[:id])
+    @have_homeworks=@user.have_homeworks.includes(:homework)
   end
 end
--- app/helpers/have_homeworks_helper.rb
+++ app/helpers/have_homeworks_helper.rb
@@ -1,2 +1,16 @@
 module HaveHomeworksHelper
+
+  def score_label(work)
+    label_class="label "
+    if work.score==4
+    label_class+="label-primary"
+    elsif work.score==3
+    label_class+="label-success"
+    elsif work.score==2
+    label_class+="label-info"
+    else
+    label_class+="label-warning"
+    end
+    content_tag(:span,work.score_text,class:label_class)
+  end
 end
